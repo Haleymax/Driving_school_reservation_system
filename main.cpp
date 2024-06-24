67cc76a6aae2e8c6e6d8c75e4c287bef56c4e974
haley <huanghongweimax@163.com>
2024-06-24 22:47:26 +0800

    修改文件

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int select = 0;
+
+    while (true){
+        cout << "======================  欢迎进入驾校预约系统  ====================="
+             << endl;
+        cout << endl << "请输入您的身份" << endl;
+        cout << "\t\t -------------------------------\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          1.学    员            |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          2.教    练            |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          3.管 理 员            |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          0.退    出            |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t -------------------------------\n";
+        cout << "输入您的选择: ";
+
+        cin >> select;   //用户选择
+        switch (select) {
+            case 1:            //学生身份
+                break;
+            case 2:            //老师身份
+                break;
+            case 3:            //管理员身份
+                break;
+            case 0:            //退出系统
+                cout << "欢迎再次使用" << endl;
+                system("pause");
+                return 0;
+            default:
+                cout << "输入有误 ， 请重新选择 ！" << endl;
+                system("pause");
+                system("cls");
+                break;
+        }
+
+    }
+    system("pause");
     return 0;
+
 }
