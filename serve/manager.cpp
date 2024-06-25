8e4ff1964f1546bd96e47bc49e18277075296eed
haley <huanghongweimax@163.com>
2024-06-25 22:56:16 +0800

    管理员类的声明与实习

--- serve/manager.cpp
+++ serve/manager.cpp
@@ -2,3 +2,39 @@
 // Created by huang on 2024/6/25.
 //
 
+#include "../headers/manager.h"
+
+//默认构造
+Manager::Manager(){
+
+}
+
+//有参构造(管理员姓名 ， 密码)
+Manager::Manager(string name , string pwd){
+
+}
+
+//选择菜单
+void Manager::operMenu(){
+
+}
+
+//添加账号
+void Manager::addPerson(){
+
+}
+
+//查看账号
+void Manager::showPerson(){
+
+}
+
+//查看车辆信息
+void Manager::showCar(){
+
+}
+
+//清空预约记录
+void Manager::clearFile(){
+
+}
