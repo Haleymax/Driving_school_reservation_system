b417ebe6ff3f7e84f82876a7599d8bb4a663f53f
haley <huanghongweimax@163.com>
2024-06-25 22:35:40 +0800

    创建学生类

--- serve/student.cpp
+++ serve/student.cpp
@@ -1,3 +1,35 @@
 //
 // Created by huang on 2024/6/24.
 //
+
+#include "../headers/student.h"
+
+//默认构造
+Student::Student() {
+
+}
+
+//有参构造(学号、姓名、密码)
+Student::Student(int id , string name , string pwd) {
+
+}
+
+//菜单界面
+void Student::operMenu() {
+
+}
+
+//申请预约
+void Student::applyOrder() {
+
+}
+
+//查看我的预约
+void Student::showMyOrder() {
+
+}
+
+//取消预约
+void Student::cancelOrder() {
+
+}
