5e1216b4c2d8b493ea211ee8489d4dc91a08d718
haley <huanghongweimax@163.com>
2024-06-25 22:52:34 +0800

    教练类的实现

---
+++ serve/coach.cpp
@@ -0,0 +1,30 @@
+//
+// Created by huang on 2024/6/25.
+//
+
+#include "../headers/coach.h"
+
+//默认构造
+Coach::Coach() {
+
+}
+
+//有参构造
+Coach::Coach(int empId , string name , string pwd) {
+
+}
+
+//菜单界面
+void Coach::operMenu(){
+
+}
+
+//查看所有预约
+void Coach::showAllorder(){
+
+}
+
+//审核预约
+void Coach::validOrder(){
+
+}
