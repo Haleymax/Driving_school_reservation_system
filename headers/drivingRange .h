09a0353ec5623fb7f2bf69ee38dd88900cfc5b14
haley <huanghongweimax@163.com>
2024-07-02 23:51:03 +0800

    声明练车场类

---
+++ headers/drivingRange .h
@@ -0,0 +1,20 @@
+//
+// Created by huang on 2024/7/2.
+//
+
+#ifndef DRIVING_SCHOOL_RESERVATION_SYSTEM_DRIVINGRANGE_H
+#define DRIVING_SCHOOL_RESERVATION_SYSTEM_DRIVINGRANGE_H
+
+#endif //DRIVING_SCHOOL_RESERVATION_SYSTEM_DRIVINGRANGE_H
+
+#pragma once
+#include "iostream"
+
+using namespace std;
+
+//练车场类
+class DrivingRange{
+public:
+    int m_RangeId;    //练车场地号
+    int m_MaxNum;     //练车场最大容量
+};
