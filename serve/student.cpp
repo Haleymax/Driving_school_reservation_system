1dac6ff4f2e237e2298d09d1464512fe3e8d3974
haley <huanghongweimax@163.com>
2024-07-03 23:41:21 +0800

    预约功能的实现

--- serve/student.cpp
+++ serve/student.cpp
@@ -49,7 +49,64 @@ void Student::operMenu() {
 
 //申请预约
 void Student::applyOrder() {
+    cout << "场地周一至周五开放" << endl;
+    cout << "请输入你的预约时间：" << endl;
+    cout << "1、周一 " << endl;
+    cout << "2、周二 " << endl;
+    cout << "3、周三 " << endl;
+    cout << "4、周四 " << endl;
+    cout << "5、周五 " << endl;
 
+    int date = 0;
+    int interval = 0;
+    int room = 0;
+
+    while (true){
+        cin >> date;
+        if (date >= 1 && date <= 5){
+            break;
+        }
+        cout << "输入有误，请重新输入" << endl;
+    }
+
+    cout << "请输入申请预约的时间段：" << endl;
+    cout << "1、上午" << endl;
+    cout << "2、下午" << endl;
+
+    while (true){
+        cin >> interval;
+        if (interval >= 1 && interval <= 2){
+            break;
+        }
+        cout << "输入有误，请重新输入" << endl;
+    }
+
+    cout << "请选择机房：" << endl;
+    cout << "1号机房容量：" << vDri[0].m_MaxNum << endl;
+    cout << "2号机房容量：" << vDri[1].m_MaxNum << endl;
+    cout << "3号机房容量：" << vDri[2].m_MaxNum << endl;
+    while (true){
+        cin >> room;
+        if (room >= 1 && room <= 3){
+            break;
+        }
+        cout << "输入有误，请重新输入" << endl;
+    }
+
+    cout << "预约成功！审核中" << endl;
+
+    ofstream ofs(ORDER_FILE , ios::app);
+    ofs << "date:" << date << " ";
+    ofs << "interval:" << interval << " ";
+    ofs << "stuId:" << this->m_Id << " ";
+    ofs << "stuName:" << this->m_Name << " ";
+    ofs << "roomId:" << room << " ";
+    ofs << "status:" << 1 << endl;
+
+    ofs.close();
+
+    system("pause");
+    system("cls");
 }
 
 //查看我的预约
