84275932af12c735879334cc61a449a5deb06ff0
haley <huanghongweimax@163.com>
2024-07-04 23:45:20 +0800

    预约类

---
+++ headers/orderFile.h
@@ -0,0 +1,31 @@
+//
+// Created by huang on 2024/7/4.
+//
+
+#ifndef DRIVING_SCHOOL_RESERVATION_SYSTEM_ORDERFILE_H
+#define DRIVING_SCHOOL_RESERVATION_SYSTEM_ORDERFILE_H
+
+#endif //DRIVING_SCHOOL_RESERVATION_SYSTEM_ORDERFILE_H
+
+#pragma once
+#include "iostream"
+#include "map"
+#include "globalFile.h"
+
+using namespace std;
+
+class OrderFile{
+public:
+
+    //构造函数
+    OrderFile();
+
+    //更新预约记录
+    void updateOrder();
+
+    //记录的容器 key ---记录的条数   value ---具体记录的键值对信息
+    map<int , map<string , string >> m_orderData;
+
+    //预约条数
+    int m_Size;
+};
