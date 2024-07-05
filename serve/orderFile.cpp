215bf9fa78e6d368d54aebda2306916bfd08b55c
haley <huanghongweimax@163.com>
2024-07-05 23:24:21 +0800

    写入预约记录

--- serve/orderFile.cpp
+++ serve/orderFile.cpp
@@ -9,12 +9,13 @@ OrderFile::OrderFile() {
     ifstream ifs;
     ifs.open(ORDER_FILE , ios::in);
 
-    string name;    //日期
+    string date;    //日期
     string interval;    //时间段
     string stuId;   //学员编号
     string stuName;     //学员姓名
     string rangId;      //场地编号
     string status;      //预约状态
 
-
+    this->m_Size = 0;   //预约记录个数
+    while (ifs >> date && ifs >> interval && ifs >> stuId && ifs >> stuName && ifs >> rangId && ifs >> status)
 }
